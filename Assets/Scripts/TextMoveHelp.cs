70d3f06af36ea3fe17afa15e56c5935444065bfa
Gennadiy Emelin <GennadiyEmelin92@gmail.com>
2022-08-24 19:02:04 +0300

    Add cats 1

--- Assets/Scripts/TextMoveHelp.cs
+++ Assets/Scripts/TextMoveHelp.cs
@@ -6,18 +6,36 @@ using UnityEngine.UI;
 public class TextMoveHelp : MonoBehaviour
 {
     [Header("Text")]
-    [SerializeField] private Text MoveText;
+    [SerializeField] private Text Text;
 
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
         {
-            MoveText.enabled = false;
+            Text.text = " ";
         }
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.CompareTag("JumpZone"))
+        {
+            Text.text = "Space - Прыжок";
+        }
 
+        if (collision.gameObject.CompareTag("AttackZone"))
+        {
+            Text.text = "ЛКМ - Стрелять";
+        }
+
+        if (collision.gameObject.CompareTag("2JumpZone"))
+        {
+            Text.text = "Space+Space - Двойной прыжок";
+        }
+
+        if (collision.gameObject.CompareTag("Cat1"))
+        {
+            Text.text = "Вручи 1-го котика бабушке";
+        }
     }
 }
