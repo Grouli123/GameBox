a794a074edb6cd6a7f160e900ffa2f242d011162
Grouli123 <69174156+Grouli123@users.noreply.github.com>
2022-08-25 01:18:51 +0300

    EnemyContrloller
    Изменение поведения вражеского персонажа

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,73 +1,139 @@
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
-{
+{  
+   [SerializeField] private GameObject _bullet;  
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private Transform _flipEnemy;
+   private float _fireRate;
+   private float _nextFire;
+
+    //[SerializeField] private Transform _firePoint;
+    [SerializeField] private Transform _pointOfRevert;    
+    [SerializeField] private Transform _player;
+
+    [SerializeField] private float _stoppingDistance;
     [SerializeField] private float _speed;
-    [SerializeField] private float _timeToRevert;
-    // [SerializeField] private Animator _anim;    
-    [SerializeField] private SpriteRenderer _sp;
 
-    [SerializeField] private Transform _target;
+    [SerializeField] private int _positionOfPatrol;
 
-    private Rigidbody2D _rb;
+    private bool _isMovingRight = true;
+    private bool _isIdle = false;
+    private bool _isAngry = false;
+    private bool _isBackToAPoint = false;
+    private bool _isShoot = false;
 
-    private const float _idleState = 0;
-    private const float _walkState = 1;
-    private const float _revertState = 2;
+    private Vector2 _enemyTarget;
 
-    private float _currentState;
-    private float _currentTimeToRevert;
+    [SerializeField] private float _currentSpeed;
 
-    [SerializeField] private bool _isHeroFound; // если поставить true, то он будет преследовать игрока,
-                                                // если поставить false, то он будет патрулировать от одного тригера
-                                                // к другому и при столкновении с одним из тригеров переключается на false,
-                                                // то есть на патрулирование.
-
-    private void Start()
+    private void Start() 
     {
-        _currentState = _walkState;
-        _currentTimeToRevert = 0;
-        _rb = GetComponent<Rigidbody2D>();
+        _fireRate = 1f;
+        _nextFire = Time.time;
 
-        _target.GetComponent<Transform>();
-        _isHeroFound = true;
+
+        _player.GetComponent<Transform>();
+        //_bullet.GetComponent<GameObject>();
+
+        _speed = _currentSpeed;
+        
+        //_enemyTarget = new Vector2(_bullet.transform.position.x, _bullet.transform.position.y);
     }
 
-    private void Update()
+    private void Update() 
     {
-        if (_isHeroFound == false){
-            if(_currentTimeToRevert >= _timeToRevert)
-            {
-                _currentTimeToRevert = 0;
-                _currentState = _revertState;
-            }
+        if (Vector2.Distance(transform.position, _pointOfRevert.position) < _positionOfPatrol && _isAngry == false)
+        {
+            _isIdle = true;
+        }    
 
-            switch(_currentState)
-            {
-                case _idleState:
-                    _currentTimeToRevert += Time.deltaTime;
-                    break;
-                case _walkState:
-                    _rb.velocity = Vector2.left * _speed;
-                    break;
-                case _revertState:
-                    _sp.flipX = !_sp.flipX;
-                    _speed *= -1;
-                    _currentState = _walkState;
-                    break;
-            }
+        if (Vector2.Distance(transform.position, _player.position) < _stoppingDistance)
+        {
+            _isAngry = true;
+            _isIdle = false;
+            _isBackToAPoint = false;
+        }
 
-            // _anim.SetFloat("Velocity",_rb.velocity.magnitude);
+        if (Vector2.Distance(transform.position, _player.position) > _stoppingDistance)
+        {
+            _isBackToAPoint = true;
+            _isAngry = false;
+        }
+
+        if (_isIdle == true)
+        {
+            Idle();
+        }
+        else if (_isAngry == true)
+        {
+            Angry();
+        }
+        else if (_isBackToAPoint)
+        {
+            BackToAPoint();
+        }
+    }
+
+    private void Idle()
+    {
+        if (transform.position.x > _pointOfRevert.position.x + _positionOfPatrol)
+        {
+            
+            _isMovingRight = false;
+            
+            _flipEnemy.transform.Rotate(0f, 180f, 0f);
+            Debug.Log("1");
+        }
+        else if (transform.position.x < _pointOfRevert.position.x - _positionOfPatrol)
+        {
+            _isMovingRight = true;
+            
+            _flipEnemy.transform.Rotate(0f, 180f, 0f);
+            Debug.Log("2");
+        }
+
+        if (_isMovingRight)
+        {
+            transform.position = new Vector2(transform.position.x + _speed * Time.deltaTime, transform.position.y);
+            
         }
         else
         {
-            transform.position = Vector2.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);    
+            transform.position = new Vector2(transform.position.x - _speed * Time.deltaTime, transform.position.y);
+
+
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) 
+    private void Angry()
     {
-        _currentState = _idleState;    
-        _isHeroFound = false;
+        transform.position = Vector2.MoveTowards(transform.position, _player.position, _speed * Time.deltaTime);
+        //transform.Rotate(0f, 180f, 0f);
+
+        _speed = _currentSpeed * 1.2f;
+        CheakIfTimeToFire();
+        
+       
+
+       
+       
+        //GameObject _currentBullet = Instantiate(_bullet, _firePoint.position, _firePoint.rotation);
+
+    }
+
+    private void BackToAPoint()
+    {        
+        transform.position = Vector2.MoveTowards(transform.position, _pointOfRevert.position, _speed * Time.deltaTime);
+        _speed = _currentSpeed;
+    }
+
+    private void CheakIfTimeToFire()
+    {
+        if (Time.time > _nextFire)
+        {
+            Instantiate(_bullet, _firePoint.transform.position, Quaternion.identity);
+            _nextFire = Time.time + _fireRate;
+        }
     }
 }
