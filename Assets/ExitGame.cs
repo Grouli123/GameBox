bf20c8cc41d307300075703e3e8f5047120ca70a
Grouli123 <69174156+Grouli123@users.noreply.github.com>
2022-08-29 21:10:52 +0300

    UI
    Настройка переключений в меню

---
+++ Assets/ExitGame.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject _globalNewGame;
+    [SerializeField] private GameObject _globalLoadGame;
+    [SerializeField] private GameObject _recordsPannel;
+    [SerializeField] private GameObject _settings;
+    [SerializeField] private GameObject _developersPannel;
+    [SerializeField] private GameObject _exitPannel;
+
+
     [SerializeField] private GameObject _startPannel;
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _startNewGame;
@@ -9,30 +17,54 @@ public class MenuController : MonoBehaviour
     [SerializeField] private GameObject _nickname;
     [SerializeField] private GameObject _levelsAndCats;
     [SerializeField] private GameObject _levels;
+    [SerializeField] private GameObject _saveLevelsTopButtons;
     [SerializeField] private GameObject _dossier;
 
-    // [SerializeField] private GameObject _loadGame;
-    // [SerializeField] private GameObject _recordsPannel;
-    // [SerializeField] private GameObject _settings;
-    // [SerializeField] private GameObject _developersPannel;
-    // [SerializeField] private GameObject _exitPannel;
-    
+    [SerializeField] private GameObject _loadGame;
+    [SerializeField] private GameObject _selectPanelSave;
+    [SerializeField] private GameObject _loadLevelsAndDossier;
+    [SerializeField] private GameObject _loadLevelSelectPanel;
+    [SerializeField] private GameObject _loadCatsDosiePanel;
 
     private void Start() 
     {
+        _globalNewGame.SetActive(true);
+        _globalLoadGame.SetActive(false);
+        _recordsPannel.SetActive(false);
+        _settings.SetActive(false);
+        _developersPannel.SetActive(false);
+        _exitPannel.SetActive(false);
+
         _startPannel.SetActive(true);
         _mainMenu.SetActive(false);
-        // _loadGame.SetActive(false);
-        // _recordsPannel.SetActive(false);
-        // _settings.SetActive(false);
-        // _developersPannel.SetActive(false);
-        // _exitPannel.SetActive(false);
+        _startNewGame.SetActive(false);
+        _slot1.SetActive(false);
+        _nickname.SetActive(false);
+        _levelsAndCats.SetActive(false);
+        _levels.SetActive(false);
+        _saveLevelsTopButtons.SetActive(false);
+        _dossier.SetActive(false);
+
+        _loadGame.SetActive(false);
+        _selectPanelSave.SetActive(false);
+        _loadLevelsAndDossier.SetActive(false);
+        _loadLevelSelectPanel.SetActive(false);
+        _loadCatsDosiePanel.SetActive(false);
+    }
+
+    private void Update() 
+    {        
+        if (Input.GetKey(KeyCode.Return)) 
+        {      
+            _startPannel.SetActive(false);
+            _mainMenu.SetActive(true);
+        }   
     }
 
     public void StartGame()
     {
-        _mainMenu.SetActive(true);
         _startPannel.SetActive(false);
+        _mainMenu.SetActive(true);
     }
 
     public void StartNewGame()
@@ -63,6 +95,8 @@ public class MenuController : MonoBehaviour
     {
         _nickname.SetActive(false);
         _levelsAndCats.SetActive(true);
+        _saveLevelsTopButtons.SetActive(true);
+        _levels.SetActive(true);
     }
 
     public void CatsDossier()
@@ -77,33 +111,72 @@ public class MenuController : MonoBehaviour
         _levels.SetActive(true);
     }
 
-    // public void LoadGame()
-    // {
-    //     _mainMenu.SetActive(false);
-    //     _loadGame.SetActive(true);
-    // }
+    public void LoadGame()
+    {
+        _mainMenu.SetActive(false);
+        _globalLoadGame.SetActive(true);
+        _loadGame.SetActive(true);
+    }
 
-    // public void Records()
-    // {
-    //     _mainMenu.SetActive(false);
-    //     _recordsPannel.SetActive(true);
-    // }
+    public void Records()
+    {
+        _mainMenu.SetActive(false);
+        _recordsPannel.SetActive(true);
+    }
 
-    // public void Settings()
-    // {
-    //     _mainMenu.SetActive(false);
-    //     _settings.SetActive(true);
-    // }
+    public void Settings()
+    {
+        _mainMenu.SetActive(false);
+        _settings.SetActive(true);
+    }
 
-    // public void Developers()
-    // {
-    //     _mainMenu.SetActive(false);
-    //     _developersPannel.SetActive(true);
-    // }
+    public void Developers()
+    {
+        _mainMenu.SetActive(false);
+        _developersPannel.SetActive(true);
+    }
 
-    // public void Exit()
-    // {
-    //     _mainMenu.SetActive(false);
-    //     _exitPannel.SetActive(true);
-    // }
+    public void Exit()
+    {
+        _mainMenu.SetActive(false);
+        _exitPannel.SetActive(true);
+    }
+
+    public void ExitTurnBack()
+    {
+        _exitPannel.SetActive(false);
+        _mainMenu.SetActive(true);
+    }
+
+    public void LoadSlot1()
+    {
+        _loadGame.SetActive(false);
+        _selectPanelSave.SetActive(true);
+    }
+
+    public void LoadSlotsTurnBack()
+    {
+        _selectPanelSave.SetActive(false);
+        _loadGame.SetActive(true);
+    }
+
+    public void LoadLevelAndDossier()
+    {
+        _selectPanelSave.SetActive(false);
+        _loadCatsDosiePanel.SetActive(false);
+        _loadLevelsAndDossier.SetActive(true);
+        _loadLevelSelectPanel.SetActive(true);
+    }
+
+    public void LoadLevels()
+    {
+        _loadCatsDosiePanel.SetActive(false);
+        _loadLevelSelectPanel.SetActive(true);        
+    }
+
+     public void LoadCatsAndDosie()
+    {
+        _loadLevelSelectPanel.SetActive(false); 
+        _loadCatsDosiePanel.SetActive(true);       
+    }
 }
