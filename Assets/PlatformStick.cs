04f552f76e6d5f7839ce2c9845f9f9675b36373c
Grouli123 <69174156+Grouli123@users.noreply.github.com>
2022-09-12 01:33:38 +0300

    Add stick material to platforms

---
+++ Assets/PlatformStick.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlatformStick : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
